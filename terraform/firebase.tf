284a29e0604c0916722100afbc0c12714fa2982d
James King <ripixel@gmail.com>
2025-12-25 12:17:43 +0000

    refactor: move Firebase API enablement from Terraform to the setup script

--- terraform/firebase.tf
+++ terraform/firebase.tf
@@ -1,27 +1,7 @@
-# Enable Firebase APIs
-resource "google_project_service" "firebase" {
-  project = var.project_id
-  service = "firebase.googleapis.com"
-
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "firebase_hosting" {
-  project = var.project_id
-  service = "firebasehosting.googleapis.com"
-
-  disable_on_destroy = false
-}
-
 # Initialize Firebase for the project
-# Note: This uses the null_resource with local-exec as there's no native Terraform resource
+# Note: Firebase APIs must be enabled first (done by bootstrap script)
+# This uses null_resource with local-exec as there's no native Terraform resource
 resource "null_resource" "firebase_init" {
-  # Only run if Firebase APIs are enabled
-  depends_on = [
-    google_project_service.firebase,
-    google_project_service.firebase_hosting
-  ]
-
   provisioner "local-exec" {
     command = "gcloud alpha firebase projects:addfirebase ${var.project_id} || true"
   }
